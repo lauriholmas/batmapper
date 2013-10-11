7d6609a9f1e106514a40adf40f7a152ff658d61a
Lauri Holmas <lholmas@cs.helsinki.fi>
2013-10-12 01:15:05 +0300

    version number of jar incremented

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 call mvn clean install
-echo y|copy target\uber-batUtils-1.0.jar ..\..\batclient\plugins
+echo y|copy target\uber-batUtils-1.1.jar ..\..\batclient\plugins
 cd C:\Users\lauri\Documents\batclient\
 call debug.bat
 cd c:\users\lauri\workspace\batUtils
