597bed81d904ff5c2a1e353a3ecdbc2da9043235
Lauri Holmas <lholmas@cs.helsinki.fi>
2013-11-10 22:17:36 +0200

    import clean

--- src/test/java/com/glaurung/batMap/gui/ManualTest1.java
+++ src/test/java/com/glaurung/batMap/gui/ManualTest1.java
@@ -1,20 +1,10 @@
 package com.glaurung.batMap.gui;
 
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.HashSet;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
 
-import com.glaurung.batMap.controller.MapperEngine;
 import com.glaurung.batMap.gui.manual.ManualPanel;
-import com.glaurung.batMap.vo.Exit;
-import com.glaurung.batMap.vo.Room;
-
-import edu.uci.ics.jung.graph.SparseMultigraph;
 
 public class ManualTest1{
 
