ad3b6881bbdb932663a99b0a64cc3b90801efe8d
lauriholmas <lauriholmas@users.noreply.github.com>
2017-03-24 22:48:57 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+# this file is run on mac with the http://www.bat.org/batclient/exe/batclient-mac-osx.pkg installed
+# ps amarth if you see this, fix that default installation dir ,)
 mvn clean install
 cp target/batUtils-1.3-full.jar ~/batclient/plugins/
 /Users/axu/bat/Batclient.app/Contents/MacOS/batclient
