17a17ae9754ad752ba89f709d9b26871d9b390c4
Lauri Holmas <lauri.holmas@lekane.com>
2018-03-08 23:47:47 +0200

    updated buildfile to use a batclient with debug

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 # this file is run on mac with the http://www.bat.org/batclient/exe/batclient-mac-osx.pkg installed
 # ps amarth if you see this, fix that default installation dir ,)
 mvn clean install
-cp target/batUtils-1.3-full.jar ~/batclient/plugins/
-/Users/axu/bat/Batclient.app/Contents/MacOS/batclient
+cp target/batUtils-1.4-SNAPSHOT-full.jar ~/batclient/plugins/
+#/Users/axu/bat/Batclient.app/Contents/MacOS/batclient
+javaws ~/Downloads/batclient.jnlp
