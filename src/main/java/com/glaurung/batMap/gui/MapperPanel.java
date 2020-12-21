3a486239f1544100366237fcb00c81d395956c97
Arafon <>
2020-12-21 10:58:04 -0500

    save and clear buttons

--- src/main/java/com/glaurung/batMap/gui/MapperPanel.java
+++ src/main/java/com/glaurung/batMap/gui/MapperPanel.java
@@ -12,6 +12,7 @@ import java.awt.geom.Point2D;
 
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.LineBorder;
@@ -61,6 +62,8 @@ public class MapperPanel extends JPanel implements ComponentListener, DocumentLi
     private boolean visibleDescs = true;
 
     private JComboBox roomColor;
+    private JButton saveButton;
+    private JButton clearButton;
     MapperEngine engine;
 
 
@@ -82,7 +85,7 @@ public class MapperPanel extends JPanel implements ComponentListener, DocumentLi
         vv.setBackground( BG_COLOR );
         vv.setBounds( 7, 7, 500, 500 );
 
-        descPanel.setBounds( 514, BORDERLINE, DESC_WIDTH, SHORT_DESC_HEIGHT + LONG_DESC_HEIGHT + EXITS_HEIGHT + 4 * BORDERLINE );
+        descPanel.setBounds( 514, BORDERLINE, DESC_WIDTH, SHORT_DESC_HEIGHT + LONG_DESC_HEIGHT + EXITS_HEIGHT + 4 * BORDERLINE  + BUTTON_HEIGHT * 2);
         descPanel.setAlignmentX( Component.RIGHT_ALIGNMENT );
         descPanel.setLayout( null );
         descPanel.setBackground( BG_COLOR );
@@ -136,6 +139,24 @@ public class MapperPanel extends JPanel implements ComponentListener, DocumentLi
         roomColor.setToolTipText( "Use this to change room color to highlight it" );
         roomColor.setRenderer( new MapperPanelCellRenderer() );
 
+
+	saveButton = new JButton( "Save" );
+	descPanel.add( saveButton );
+	saveButton.setFont( font );
+	saveButton.setBounds( 0,
+			      SHORT_DESC_HEIGHT + BORDERLINE + LONG_DESC_HEIGHT + BORDERLINE + EXITS_HEIGHT + BORDERLINE + BUTTON_HEIGHT + BORDERLINE, BUTTON_WIDTH, BUTTON_HEIGHT );
+	saveButton.setToolTipText( "Save the map to a file." );
+	saveButton.addActionListener( this );
+
+	clearButton = new JButton( "Clear" );
+	descPanel.add( clearButton );
+	clearButton.setFont( font );
+	clearButton.setBounds( BUTTON_WIDTH + BORDERLINE,
+			       SHORT_DESC_HEIGHT + BORDERLINE + LONG_DESC_HEIGHT + BORDERLINE + EXITS_HEIGHT + BORDERLINE + BUTTON_HEIGHT + BORDERLINE, BUTTON_WIDTH, BUTTON_HEIGHT );
+	clearButton.setToolTipText( "Clear the map for this area." );
+	clearButton.addActionListener( this );
+
+
 //		roomNotes.setBounds(0, BORDERLINE+SHORT_DESC_HEIGHT+BORDERLINE+LONG_DESC_HEIGHT+BORDERLINE+EXITS_HEIGHT+BORDERLINE+BUTTON_HEIGHT+BORDERLINE, DESC_WIDTH, NOTES_HEIGHT);
         roomNotes.setEditable( true );
         roomNotes.setColumns( 25 );
@@ -148,8 +169,8 @@ public class MapperPanel extends JPanel implements ComponentListener, DocumentLi
 
 //		descPanel.add(roomNotes);
         roomNotes.setToolTipText( "Feel free to write your own notes here." );
-        scrollableNotes.setBounds( 514, SHORT_DESC_HEIGHT + BORDERLINE + LONG_DESC_HEIGHT + BORDERLINE + EXITS_HEIGHT + BORDERLINE + BUTTON_HEIGHT + BORDERLINE, DESC_WIDTH, descPanel.getHeight() - ( 4 * BORDERLINE + LONG_DESC_HEIGHT + EXITS_HEIGHT + SHORT_DESC_HEIGHT + BUTTON_HEIGHT ) );
-        this.add( scrollableNotes );
+        scrollableNotes.setBounds( 514, SHORT_DESC_HEIGHT + BORDERLINE + LONG_DESC_HEIGHT + BORDERLINE + EXITS_HEIGHT + BORDERLINE + BUTTON_HEIGHT + BORDERLINE + BUTTON_HEIGHT + BORDERLINE, DESC_WIDTH, descPanel.getHeight() - ( 4 * BORDERLINE + LONG_DESC_HEIGHT + EXITS_HEIGHT + SHORT_DESC_HEIGHT + BUTTON_HEIGHT + BUTTON_HEIGHT) );
+        //this.add( scrollableNotes );
 
 
         this.setLayout( null );
@@ -175,7 +196,7 @@ public class MapperPanel extends JPanel implements ComponentListener, DocumentLi
 
         if (visibleDescs) {
             vv.setBounds( 7, BORDERLINE, this.getWidth() - ( DESC_WIDTH + 21 ), this.getHeight() - ( BORDERLINE + 7 ) );
-            descPanel.setBounds( this.getWidth() - ( 7 + DESC_WIDTH ), BORDERLINE, DESC_WIDTH, SHORT_DESC_HEIGHT + LONG_DESC_HEIGHT + EXITS_HEIGHT + BUTTON_HEIGHT + 4 * 7 );
+            descPanel.setBounds( this.getWidth() - ( 7 + DESC_WIDTH ), BORDERLINE, DESC_WIDTH, SHORT_DESC_HEIGHT + LONG_DESC_HEIGHT + EXITS_HEIGHT + BUTTON_HEIGHT*2 + 4 * 7 );
             scrollableNotes.setBounds( this.getWidth() - ( 7 + DESC_WIDTH ), BORDERLINE + descPanel.getHeight() + 7, DESC_WIDTH, this.getHeight() - ( descPanel.getHeight() + 14 + BORDERLINE ) );
 
         } else {
@@ -248,8 +269,13 @@ public class MapperPanel extends JPanel implements ComponentListener, DocumentLi
     public void actionPerformed( ActionEvent e ) {
         if (e.getSource().equals( roomColor )) {
             this.engine.changeRoomColor( RoomColors.getColors()[roomColor.getSelectedIndex()] );
-        }
-
+        } else if (e.getSource().equals( saveButton )) {
+	    System.out.println("pressed save.");
+	    engine.save();
+	} else if (e.getSource().equals( clearButton )) {
+	    System.out.println("pressed clear.");
+	    engine.clearCurrentArea();
+	}
     }
 
 
